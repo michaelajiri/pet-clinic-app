13f83c7e595ae31da67ada42d3ea59ef186311f6
michaelajiri89 <michaelajiri@gmail.com>
2020-09-30 09:19:17 +0100

    Added WRO4J plugin and config from Spring Pet Clinic. Closes #24

--- pet-clinic-web/src/main/java/com/infotech/petclinicapp/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/infotech/petclinicapp/controllers/VetController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/vets")
 public class VetController {
 
     private final VetService vetService;
@@ -16,7 +15,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping(value = {"", "/", "/index", "/index.html"})
+    @RequestMapping(value = {"/vets", "/vets/index", "/vets/index.html"})
     public String vetPage(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
